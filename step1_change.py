3078c5a907870c86ab89c148ab334aedcc53aa06
zzy0987 <lwzhangzy2000@163.com>
2025-09-30 16:34:35 +0000

    update

--- step1_change.py
+++ step1_change.py
@@ -77,9 +77,9 @@ def process_data(json_path, output_json_path, target_root):
 
 if __name__ == "__main__":
     # 假设原始 JSON 路径
-    json_file = "/data_sdg/sunz/FragFake/finished_file/dataset/ADE20K/step1x/hard/result/step1x_ADE20K_gemma3_hard.json"
+    json_file = "/data_sdg/sunz/FragFake/finished_file/dataset/ADE20K/flux/easy/result/flux_ADE20K_gemma3_easy.json"
     # 输出更新后 JSON 的路径
-    output_json_file = "zzy_anno_step1x_ADE20K_gemma3_hard.json"
+    output_json_file = "zzy_anno_flux_ADE20K_gemma3_easy.json"
     # 搬运图片的目标根目录
     target_root_folder = "/data_sda/zzy/pic_anno"
 
